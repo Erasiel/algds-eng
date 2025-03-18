2837fd7d44594fe5b657d400ad7740bff2427166
Erasiel <abalogh@inf.u-szeged.hu>
2025-03-18 18:54:31 +0100

    Fixed miscalculations

--- sample_tests/sample_test_01/src/task04.py
+++ sample_tests/sample_test_01/src/task04.py
@@ -9,6 +9,6 @@ def rpn_eval(rpn: List[Union[str, float]], n: int) -> float:
 if __name__ == "__main__":
     assert rpn_eval([5.0, 4.0, "*", 3.0, "+"], 5) == 23
     assert rpn_eval([5.0, 4.0, 3.0, "*", "+"], 5) == 17
-    assert rpn_eval([5.0, 4.0, "*", 3.0, 2.0, "*", "+"], 7) == 32
+    assert rpn_eval([5.0, 4.0, "*", 3.0, 2.0, "*", "+"], 7) == 26
     assert rpn_eval([5.0, 3.0, "+", 4.0, 2.0, "-", "/"], 7) == 4
     assert rpn_eval([1.0, 2.0, "+", 3.0, "*", 6.0, "+", 8.0, 5.0, "-", "/"], 11) == 5
